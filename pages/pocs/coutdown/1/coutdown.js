e7cbfddf130be4d82d9b0243344c157aa5db3313
Damian Sire <damiansire@hotmail.com>
2023-08-21 18:04:22 -0300

    feat: add poc for coutdown

---
+++ pages/pocs/coutdown/1/coutdown.js
@@ -0,0 +1,26 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const mins = document.getElementById("mins");
+const seconds = document.getElementById("seconds");
+
+const newYears = "1 Jan 2024";
+
+coutDown();
+
+function coutDown(e) {
+  const newYearsDate = new Date(newYears);
+  const currentDay = new Date();
+
+  const totalSec = (newYearsDate - currentDay) / 1000;
+
+  const sec = Math.floor(totalSec) % 60;
+  const min = Math.floor(totalSec / 60) % 60;
+  const h = Math.floor(totalSec / 3600) % 24;
+  const d = Math.floor(totalSec / 3600 / 24);
+  days.innerHTML = `<h1>${d}</h1><h2>days</h2>`;
+  hours.innerHTML = `<h1>${h}</h1><h2>hours</h2>`;
+  mins.innerHTML = `<h1>${min}</h1><h2>mins</h2>`;
+  seconds.innerHTML = `<h1>${sec}</h1><h2>seconds</h2>`;
+}
+
+setInterval(coutDown, 1000);
