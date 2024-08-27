21bff4a715db3cfa26c8c7c4fcb95a6aa28cf03b
OWU Uruguay <openweburuguay@gmail.com>
2024-08-26 23:37:57 -0300

    fix: Add accent to interés

--- src/app/(web)/(content)/(meetups)/(2024)/la-meetup/interes/page.tsx
+++ src/app/(web)/(content)/(meetups)/(2024)/la-meetup/interes/page.tsx
@@ -5,7 +5,7 @@ export default function Intrest() {
     "https://docs.google.com/forms/d/e/1FAIpQLSe7QPkYcoz1tZ-j4N-BvqNfkLMtp9Oq8vQ285mTR7EuJPDVnw/viewform?embedded=true";
 
   return (
-    <EmbeddedFormLayout title="¡Formulario de Interes!">
+    <EmbeddedFormLayout title="¡Formulario de Interés!">
       <iframe
         className="iframeembed flex min-h-[70rem] w-full max-w-[750px] flex-1 flex-col md:min-h-[60rem]"
         src={url}
