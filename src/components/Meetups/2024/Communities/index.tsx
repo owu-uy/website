c20738fe58171b1b0482be074e6b1244a38645c3
OWU Uruguay <openweburuguay@gmail.com>
2024-09-11 22:14:11 -0300

    feat: Remove add community btn (#63)
    Co-authored-by: Agustín Tornielli <agustin.tornielli@streaver.com>

--- src/components/Meetups/2024/Communities/index.tsx
+++ src/components/Meetups/2024/Communities/index.tsx
@@ -52,12 +52,12 @@ export default function Communities({ title, subtitle, communities = [] }: Commu
           ))}
         </motion.div>
       </div>
-      <Link
+      {/* <Link
         className="inline-flex w-full max-w-[280px] skew-x-[-21deg] cursor-pointer items-center justify-center self-center border-2 border-yellow-400 px-5 py-2.5 text-base font-semibold uppercase text-white ease-in before:absolute before:-inset-0.5 before:origin-right before:scale-x-0 before:bg-yellow-400 hover:scale-110 hover:text-black hover:before:origin-left hover:before:scale-x-100 aria-disabled:pointer-events-none aria-disabled:border-[#666] aria-disabled:bg-[#666] aria-disabled:text-[#111] motion-safe:transition-[color,transform] motion-safe:before:transition-transform motion-safe:before:duration-300 motion-safe:before:ease-in motion-safe:hover:delay-100 motion-safe:hover:ease-out motion-safe:hover:before:delay-100 motion-safe:hover:before:ease-out"
         href="/la-meetup/comunidades"
       >
         <span className="inline-flex skew-x-[21deg] items-center justify-center text-center">¡SUMÁ TU COMUNIDAD!</span>
-      </Link>
+      </Link> */}
     </div>
   );
 }
