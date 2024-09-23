d45b3602d90f07cd7bdbc218ac377ec69f318884
OWU Uruguay <openweburuguay@gmail.com>
2024-09-22 22:15:22 -0300

    feat: Remove add community btn (#63) (#67)
    Co-authored-by: Agustín Tornielli <agustin.tornielli@streaver.com>

--- src/components/Meetups/2024/Ticket/index.tsx
+++ src/components/Meetups/2024/Ticket/index.tsx
@@ -63,7 +63,7 @@ export default function Ticket({ sponsors }: TicketProps) {
           <p className="text-xs font-semibold text-yellow-400 lg:absolute lg:right-4 lg:top-6">19.10.24</p>
           <p className="text-gray-400/150 text-xs text-gray-400 lg:absolute lg:right-4 lg:top-11">SINERGIA FARO</p>
           <img alt="OWU Uruguay" className="mt-4 max-w-[90px] object-cover lg:max-w-[190px]" src="/ticket_logo.webp" />
-          <div className="flex w-full flex-row items-start gap-2">
+          <div className="flex w-full flex-row items-center gap-2">
             {currentSponsors.map((sponsor: Sponsor) => (
               <img
                 key={sponsor.id}
