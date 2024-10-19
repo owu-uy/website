a5fc0ec4f8e1c25fd26fe6c748ea63e0985f9056
Agustín Tornielli <agustin.tornielli@streaver.com>
2024-10-19 11:38:38 -0300

    Fix: Agenda

--- src/app/(web)/(content)/layout.tsx
+++ src/app/(web)/(content)/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     <NavigationProvider>
       <Starfield speedFactor={0.05} starColor={[255, 255, 255]} starCount={1000} />
       <Navbar />
-      <section className="relative z-50 flex flex-col">{children}</section>
+      <section className="max-w-screen relative z-50 flex flex-col">{children}</section>
     </NavigationProvider>
   );
 }
