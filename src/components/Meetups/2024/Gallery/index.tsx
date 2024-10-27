1f60edfd614eca53fbf536b2940427dede625d70
Agustín Tornielli <agustin.tornielli@streaver.com>
2024-10-27 19:01:11 -0300

    fix: Gallery arrows position

--- src/components/Meetups/2024/Gallery/index.tsx
+++ src/components/Meetups/2024/Gallery/index.tsx
@@ -46,8 +46,8 @@ export default function Gallery({ gallery }: GalleryProps) {
           ))}
         </CarouselContent>
 
-        <CarouselPrevious className="md:left-[70px]" />
-        <CarouselNext className="md:right-[70px]" />
+        <CarouselPrevious />
+        <CarouselNext />
       </Carousel>
       <Link
         className="inline-flex w-full max-w-[280px] skew-x-[-21deg] cursor-pointer items-center justify-center self-center border-2 border-yellow-400 px-5 py-2.5 text-base font-semibold uppercase text-white ease-in before:absolute before:-inset-0.5 before:origin-right before:scale-x-0 before:bg-yellow-400 hover:scale-110 hover:text-black hover:before:origin-left hover:before:scale-x-100 aria-disabled:pointer-events-none aria-disabled:border-[#666] aria-disabled:bg-[#666] aria-disabled:text-[#111] motion-safe:transition-[color,transform] motion-safe:before:transition-transform motion-safe:before:duration-300 motion-safe:before:ease-in motion-safe:hover:delay-100 motion-safe:hover:ease-out motion-safe:hover:before:delay-100 motion-safe:hover:before:ease-out"
