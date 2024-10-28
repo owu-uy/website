da62aba61ed9eaff7a5a76201f458f302be873cc
OWU Uruguay <openweburuguay@gmail.com>
2024-10-28 01:03:25 -0300

    fix: View more btn

--- src/components/Meetups/2024/Gallery/index.tsx
+++ src/components/Meetups/2024/Gallery/index.tsx
@@ -50,13 +50,11 @@ export default function Gallery({ gallery }: GalleryProps) {
         <CarouselNext />
       </Carousel>
       <Link
-        className="inline-flex w-full max-w-[280px] skew-x-[-21deg] cursor-pointer items-center justify-center self-center border-2 border-yellow-400 px-5 py-2.5 text-base font-semibold uppercase text-white ease-in before:absolute before:-inset-0.5 before:origin-right before:scale-x-0 before:bg-yellow-400 hover:scale-110 hover:text-black hover:before:origin-left hover:before:scale-x-100 aria-disabled:pointer-events-none aria-disabled:border-[#666] aria-disabled:bg-[#666] aria-disabled:text-[#111] motion-safe:transition-[color,transform] motion-safe:before:transition-transform motion-safe:before:duration-300 motion-safe:before:ease-in motion-safe:hover:delay-100 motion-safe:hover:ease-out motion-safe:hover:before:delay-100 motion-safe:hover:before:ease-out"
+        className="inline-flex w-full max-w-[180px] skew-x-[-21deg] cursor-pointer items-center justify-center self-center border-2 border-yellow-400 px-5 py-2.5 text-base font-semibold uppercase text-white ease-in before:absolute before:-inset-0.5 before:origin-right before:scale-x-0 before:bg-yellow-400 hover:scale-110 hover:text-black hover:before:origin-left hover:before:scale-x-100 aria-disabled:pointer-events-none aria-disabled:border-[#666] aria-disabled:bg-[#666] aria-disabled:text-[#111] motion-safe:transition-[color,transform] motion-safe:before:transition-transform motion-safe:before:duration-300 motion-safe:before:ease-in motion-safe:hover:delay-100 motion-safe:hover:ease-out motion-safe:hover:before:delay-100 motion-safe:hover:before:ease-out md:max-w-[280px]"
         href="https://www.linkedin.com/company/owu-uruguay/posts/"
         target="_blank"
       >
-        <span className="inline-flex skew-x-[21deg] items-center justify-center text-center">
-          ¡VER MÁS EN LINKEDIN!
-        </span>
+        <span className="inline-flex skew-x-[21deg] items-center justify-center text-center">¡VER MÁS!</span>
       </Link>
     </div>
   );
